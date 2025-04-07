62c172604ce59efda5895452e48225b90b9b30de
Ainhoa <123171530+RogueBio@users.noreply.github.com>
2025-04-07 11:53:47 +0100

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-#SBATCH -p short          # Partition name
-#SBATCH -t 0-2:00         # Time limit (D-HH:MM)
-#SBATCH -c 6              # Number of cores
-#SBATCH --mem 6G          # Requested memory
-#SBATCH -o %x_%j.out      # File to which stdout will be written
-#SBATCH -e %x_%j.err      # File to which stderr will be written
+#SBATCH --partition=cpu-standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50G
 
 # Load modules required for script commands
-module load fastqc/0.12.1
+module load FastQC/0.12.1-Java-17 
 
 # Check if variables are passed correctly
-if [ -z "$fastq_file" ] || [ -z "$output_dir" ]; then
-    echo "Error: Required variables 'fastq_file' and 'output_dir' are not set."
+if [ -z "$fastq_file" ] || [ ! -f "$fastq_file" ]; then
+    echo "Error: FASTQ file '$fastq_file' is either not set or does not exist."
     exit 1
 fi
 
-# Ensure the output directory exists
-mkdir -p "$output_dir"
-
 # Run FASTQC
-fastqc -o "$output_dir" -t "$SLURM_CPUS_PER_TASK" "$fastq_file"
+fastqc -o "$output_dir" "$fastq_file"
 
 # Archive the results in the output directory
 cd "$output_dir"
-zip -r fastqc_reports_$SLURM_JOB_ID.zip *.html *.zip
+zip -r "fastqc_reports_${SLURM_JOB_ID}.zip" *.html *.zip
